a62b8cdf3cf83bb76cc4b80db4a02d0651732525
million <74927604+Million-art@users.noreply.github.com>
2024-03-06 17:11:24 +0300

    clerk error solved

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -5,7 +5,7 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
-//importting user models
+//importing user models
 import { createUser, deleteUser, updateUser } from "../../../../lib/actions/user.actions";
 
 export async function POST(req: Request) {
