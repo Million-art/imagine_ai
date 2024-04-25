c41c6ff01e0f1eb0f67630e03060e47f6c9b9680
million-art <millionmulugeta09@gmail.com>
2024-04-25 16:00:42 +0300

    minor error fix

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -10,7 +10,7 @@ import { createUser, deleteUser, updateUser } from "../../../../lib/actions/user
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECREAT;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -9,23 +9,33 @@ interface MongooseConnection {
 
 let cached: MongooseConnection = (global as any).mongoose
 
-if(!cached) {
+if (!cached) {
   cached = (global as any).mongoose = { 
     conn: null, promise: null 
   }
 }
 
 export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if (!MONGODB_URL) {
+    console.error('Missing MONGODB_URL');
+    throw new Error('Missing MONGODB_URL');
+  }
 
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
       dbName: 'imaginify', bufferCommands: false 
-    })
-   cached.conn = await cached.promise;
+    });
+
+  try {
+    cached.conn = await cached.promise;
+    console.log('Connected to MongoDB successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
+  }
 
   return cached.conn;
 }
